ac5f14adfe779fe7ac5db44925d8a77ff61ca205
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-03 18:19:51 -0300

    Create exercise3.js

---
+++ Section 09 - Introduction to Javascript ES6/exercise3.js
@@ -0,0 +1,4 @@
+var tweet = prompt("What are you thinking?");
+var size = tweet.length
+
+window.alert("You have written a tweet of " + size + " characters. You have left " + (280 - size) + " characters");
