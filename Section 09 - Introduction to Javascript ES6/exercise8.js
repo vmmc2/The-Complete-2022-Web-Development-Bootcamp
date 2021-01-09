50a445c17a94f2a182e51dfcc4dfc1e55ceaf007
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-09 12:02:39 -0300

    Create exercise8.js

---
+++ Section 09 - Introduction to Javascript ES6/exercise8.js
@@ -0,0 +1,24 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+
+ 
+function main(){
+   dropBeeper();
+   dropBeeper();
+   dropBeeper();
+   dropBeeper();
+}
+
+function dropBeeper(){
+   putBeeper();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   turnLeft();
+   turnLeft();
+}
+
