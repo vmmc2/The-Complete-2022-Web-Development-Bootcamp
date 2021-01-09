d7cfce9a4283c9f96dc785b78a046ac9b76735c6
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-09 11:57:03 -0300

    Create exercise7.js

---
+++ Section 09 - Introduction to Javascript ES6/exercise7.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+
+ 
+function main(){
+   move();
+   move();
+   move();
+   move();
+   turnLeft();
+   move();
+   move();
+   move();
+   move();
+}
+
+
