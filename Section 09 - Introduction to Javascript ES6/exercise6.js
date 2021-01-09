8e30c3bc70c0883dc2f0661cd84fa95157663e9d
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-09 10:36:10 -0300

    Create exercise6.js

---
+++ Section 09 - Introduction to Javascript ES6/exercise6.js
@@ -0,0 +1,4 @@
+var dogAgeStr = prompt("Insert your dog age: ");
+var dogAge = parseInt(dogAgeStr, 10);
+var humanAge = ((dogAge - 2) * 4) + 21
+window.alert("Your dog has a human age of: " + humanAge);
