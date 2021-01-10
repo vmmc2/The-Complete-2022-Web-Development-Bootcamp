c92e2a58616b5c56388a70a795679460a6789ceb
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-10 15:50:41 -0300

    Create exercise10.js

---
+++ Section 09 - Introduction to Javascript ES6/exercise10.js
@@ -0,0 +1,17 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var years = 90 - age;
+    var months = years * 12;
+    var weeks = years * 52;
+    var days = years * 365;
+    
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
