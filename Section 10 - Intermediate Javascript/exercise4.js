a36f7a268752707081825c29e425220b0151c78b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-01-10 18:21:33 -0300

    Create exercise4.js

---
+++ Section 10 - Intermediate Javascript/exercise4.js
@@ -0,0 +1,16 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    var rnd = Math.random();
+    var index = Math.floor(rnd * names.length);
+    
+    return names[index] + " is going to buy lunch today!";
+    
+    
+    
+
+
+/******Don't change the code below*******/    
+}
