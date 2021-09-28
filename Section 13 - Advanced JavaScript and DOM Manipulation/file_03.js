6992423f05f16b45f15ea05b2c71d8b80cb8e992
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-09-28 15:01:21 -0300

    Update file_03.js

--- Section 13 - Advanced JavaScript and DOM Manipulation/file_03.js
+++ Section 13 - Advanced JavaScript and DOM Manipulation/file_03.js
@@ -2,11 +2,16 @@
 //JavaScript permite a criacao de objetos, assim como outras linguagens
 //tambem o permitem como: C++, Python, Java, Dart, etc...
 
-//Segue abaixo um exemplo de criacao de objeto:
+//Segue abaixo um exemplo de criacao de objeto (com atributos e metodos):
 var bellboy1 = {
     name: "William",
     age: 19,
     hasWorkPermit: true,
+    moveLuggage: function(){
+        console.log("May I move your luggage");
+        move();
+        console.log("Your luggage has already been moved to your room.");
+    }
 };
 //Como eh possivel observar, objetos em JS sao mt parecidos aos dicts de Python
 //e aos maps de C++.
@@ -14,6 +19,7 @@ var bellboy1 = {
 console.log(bellboy1.name);
 console.log(bellboy1.age);
 console.log(bellboy1.hasWorkPermit);
+bellboy1.moveLuggage();
 
 //Eh possivel tambem utilizar uma especie de construtor para automatizar 
 //a criacao de objetos em JavaScript. Essa especie de construtor se chama:
@@ -22,5 +28,10 @@ function BellBoy(name, age, hasWorkPermit){ //O nome da funcao deve estar capita
     this.name = name;
     this.age = age;
     this.hasWorkPermit = hasWorkPermit;
+    this.moveLuggage = function(){
+        console.log("May I move your luggage");
+        move();
+        console.log("Your luggage has already been moved to your room.");
+    }
 }
 var bellboy2 = new BellBoy("Victor", 23, false);
